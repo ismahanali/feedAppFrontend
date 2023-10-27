db66e6966c24500c73ffcc9bde1cca7b253edac4
Ismahan <main@ismahans-mbp.lan>
2023-10-27 11:18:58 -0400

    added AppContext.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./appRoutes/AppRoutes";
+import AppContextProvider from "./context/applicationContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <AppRoutes />
-  </Router>
+  <AppContextProvider>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </AppContextProvider>
 );
