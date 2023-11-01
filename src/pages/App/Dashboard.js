7279b1d444043bba40d2266b2b1850ea78d5d0e6
Ismahan <main@ismahans-mbp.lan>
2023-11-01 10:59:45 -0400

    added my profile component to dashboard

--- src/pages/App/Dashboard.js
+++ src/pages/App/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext, useState } from "react";
 
 import { AppContext } from "../../context/applicationContext";
 import LoadingIndicator from "../../components/LoadingIndicator";
-
+import MyProfile from "../../components/MyProfile";
 import { getOthersFeedsApi } from "../../util/ApiUtil";
 
 const Dashboard = () => {
@@ -80,6 +80,7 @@ const Dashboard = () => {
   return (
     <main className="grid grid-cols-1 lg:grid-cols-2 gap-6 my-12 mx-0 md:mx-12 w-2xl container px-2 mx-auto">
       {/* {#MyProfile Component} */}
+      <MyProfile />
       <article>
         {/* {#AddFeed Component} */}
         {/* {#FeedCard Component} */}
